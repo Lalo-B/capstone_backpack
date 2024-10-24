789a69747492aa1c8ee9feca5d01e3b4d609917e
Gerardo_Bonilla_Jr <g830bo@gmail.com>
2024-10-24 12:38:23 -0700

    added carousel and images

--- react-vite/src/components/HomePage/Carousel.jsx
+++ react-vite/src/components/HomePage/Carousel.jsx
@@ -1,8 +1,11 @@
 import React, {useEffect, useState} from "react";
 import "./HomePage.css";
+import '/flashcards.png';
+import '/tests.png';
+import '/signup.png';
 
 const Carousel = () => {
-    const data = ["1", "2", "3", "4"];
+    const data = ["1", "2", "3"];
     const [currentIndex, setCurrentIndex] = useState(0)
     const carouselInfiniteScroll = () => {
         if (currentIndex === data.length - 1) {
@@ -11,18 +14,21 @@ const Carousel = () => {
         return setCurrentIndex(currentIndex + 1)
     }
     useEffect(() => {
-        const interval = setInterval(() => { carouselInfiniteScroll() }, 3000)
+        const interval = setInterval(() => { carouselInfiniteScroll() }, 4000)
         //clean up function
         return () => clearInterval(interval)
     })
 
     return (
         <div className='carousel-container'>
-            {data.map((item, index) => {
+            {/* {data.map((item, index) => {
                 return <h1 className='carousel-item'
                     style={{ transform: `translate(-${currentIndex * 100}%)` }}
                     key={index}>{item}</h1>
-            })}
+            })} */}
+            <img src='./flashcards.png' className="carousel-images carousel-item" style={{ transform: `translate(-${currentIndex * 100}%)` }}/>
+            <img src='./tests.png' className="carousel-images carousel-item" style={{ transform: `translate(-${currentIndex * 100}%)` }}/>
+            <img src='./signup.png' className="carousel-images carousel-item" style={{ transform: `translate(-${currentIndex * 100}%)` }}/>
         </div>
     )
 }
